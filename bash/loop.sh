522e308e186c598d589309896b3d375f1f0addaa
Aileen C <ailanigc9@gmail.com>
2019-03-26 15:47:42 -0500

    Bash script exercises
    Signed-off-by: Aileen C <ailanigc9@gmail.com>

---
+++ bash/loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# a list of service commands
+COMMANDS=( reload restart )
+
+for COMMAND in "#${COMMANDS[@]}"
+do
+  echo $COMMAND
+done
+
+# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+for FILENAME in $VHOSTS_PATH
+do
+  echo $FILENAME
+done
